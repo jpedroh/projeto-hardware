2d732c0e82d42c1397efc68d90b6a0a0a4519be0
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-03-12 13:44:57 -0300

    adicionar componentes providos pela especificacao

---
+++ src/ProvidedComponents/RegDesloc.vhd
@@ -0,0 +1,25 @@
+
+        {
+            "id": "814631019994546216",
+            "name": "Chefia da FIR Amazônica",
+        },
+        {
+            "id": "814631152564174888",
+            "name": "Chefia da FIR Recife",
+        },
+        {
+            "id": "814631180489064499",
+            "name": "Chefia da FIR Curitiba",
+        },
+        {
+            "id": "814631202186723438",
+            "name": "Chefia da FIR Brasília",
+        },
+        {
+            "id": "814631220633403402",
+            "name": "Chefia da FIR Atlântico",
+        },
+        {
+            "id": "814631238614777868",
+            "name": "Chefia do SRPV São Paulo",
+        }
