73196034ca0888fb564ca7b61948ed537121a4fd
Matheus Lucena <47263211+matheusepitacio@users.noreply.github.com>
2021-03-14 16:32:10 -0300

    corrigindo...

--- Multiplier/multiplier.v
+++ Multiplier/multiplier.v
@@ -8,18 +8,6 @@ reg [63:0] Paux; // P auxiliar na hora de montar o P
 reg [31:0] operand1compl2; //operando1 negativo
 reg [4:0] i; // i para o for
 
-if (reset == 1){ //limpando os registradores
-	A = 65'b0;
-	S = 65'b0;
-	P = 65'b0;
-	Paux = 64'b0;
-	operand'compl2 = 32'b0;
-	i = 5'b0;
-	hi = 32'b0;
-	lo = 32'b0;
-}
-
-
 //algoritmo baseado no algoritmo de multiplicacao de booth
 
 always@(posedge clock) begin
@@ -28,8 +16,18 @@ always@(posedge clock) begin
 	S = {operand1compl2, 33'b0}; // S = negativo_operando1 + zeros
 	Paux = {32'b0, operand2}; //zeros + operando2 
 	P = {Paux, 1'b0}; //Paux + 0 no fim
-	
-
+	if (reset == 1)begin //limpando os registradores
+		A = 65'b0;
+		S = 65'b0;
+		P = 65'b0;
+		Paux = 64'b0;
+		operand1compl2 = 32'b0;
+		i = 5'b0;
+		hi = 32'b0;
+		lo = 32'b0;
+	end 
+	else begin
+		
 	for(i=0; i < 32; i = i + 1) begin //itera 32 vezes pq e um numero de 32 bits
 		case(P[1:0]) //faz um case com os 2 ultimos bits de P
 				2'b00: begin 
@@ -50,6 +48,8 @@ always@(posedge clock) begin
 		end
 	assign hi = P[64:33]; //hi e os 32 primeiros bits do produto
 	assign lo = P[33:1]; // e lo e os 32 bits seguintes
+	end
+
 	
 end
 endmodule
