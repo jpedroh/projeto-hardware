3024b33e26ce47ba4e761c98db36dda0aac3f3e5
Matheus Lucena <47263211+matheusepitacio@users.noreply.github.com>
2021-03-17 19:30:18 -0300

    Multiplier funcionando e testado

--- Multiplier/multiplier.v
+++ Multiplier/multiplier.v
@@ -1,54 +1,49 @@
-module multiplier(
-	input clock,
-	input reset,
-	input start,
-	input [31:0] operando1,
-	input [31:0] operando2,
-	output [31:0] hi,
-	output [31:0] lo,
-	output fim
-);
+module divisor(fim,operand1,operando2,start,clock, hi, lo, reset); // P
 
-reg ativo;
-reg [4:0] cicloAtual;
-reg [31:0] A;
-reg [31:0] S;
-reg [64:0] P;
+   input [31:0] operand1, operando2;
+   input start, clock, reset;
+   output fim;
+   output hi;
+   output lo;
 
-wire [64:0] soma = P + {A, 33'd0};
-wire [64:0] sub = P + {S, 33'd0};
+	reg [63:0] product; //registrador que guarda as operacoes
+   reg [31:0] hi;
+   reg [31:0] lo;
+   reg [5:0] cicloAtual; 
+	wire fim = !cicloAtual; //cabo que sinaliza o fim
+   reg lostbit;
+   
+   initial cicloAtual = 0; //comeca com 0 ciclos e vai ate 32
 
-assign fim = ~ativo;
-assign hi = P[64:33];
-assign lo = P[32:1];
-
-always @(posedge clock, posedge reset) begin
-	if (reset) begin
-
+   always @( posedge clock ) begin
+   
+	   if (reset) begin //reset que limpa os registradores internos
+		product = 63'b0;
+		hi = 31'b0;
+		lo = 31'b0;
+		cicloAtual = 6'b0;
+		lostbit = 0;
 	end
-	else if (start) begin
-		if (ativo) begin
-			if (P[1:0] == 2'b01) begin
-				P <= soma;
-			end
-			if (P[1:0] == 2'b10) begin
-				P <= sub;
-			end			
-			P <= P >>> 1;
-			if (!cicloAtual) begin
-				ativo <= 0;
-			end
-			cicloAtual <= cicloAtual - 5'd1;
-		end
-		else begin
-			A <= operando1;
-			S <= -operando1;
-			P <= {{32'b0, operando2},1'b0};
-			cicloAtual <= 5'd31;
-			ativo <= 1;
-		end
-	end
-end
+
+	   if( fim && start ) begin //"inicializador" que comeca os valores
+
+        cicloAtual = 32;
+        product = { 32'd0, operando2 };
+        lostbit = 0;
+        
+     end else if( cicloAtual ) begin
+
+	     case ( {product[0],lostbit} ) //case com o P[1:0]
+		     2'b01: product[63:32] = product[63:32] + operand1; // P + A
+		     2'b10: product[63:32] = product[63:32] - operand1; // P + S
+        endcase
+
+	     lostbit = product[0]; //salva o sinal ultimo bit q vai ser perdido no shift right
+	     product = { product[63], product[63:1] }; //faz o shift right salvando o sinal
+        cicloAtual = cicloAtual - 1; //decrementa o ciclo
+	     hi = product[63:32]; //pega os primeiros 32 bits do produto
+	     lo = product[31:0]; // pega os ultimos 32 bits do produto
+     end
+   end
 
 endmodule
-	
