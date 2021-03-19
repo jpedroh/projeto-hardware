fd7fa2632d5b876b261586edbc1a13b5d61e3abe
Matheus Lucena <47263211+matheusepitacio@users.noreply.github.com>
2021-03-18 22:00:26 -0300

    corrigido e testado

--- src/Muxes/MuxALUSrcB.v
+++ src/Muxes/MuxALUSrcB.v
@@ -1,22 +1,23 @@
 module MuxALUSrcB (
-    input reg[31:0] RegDataB,
-    input reg[31:0] MemDataReg
+    input wire[31:0] RegDataB,
+    input wire[31:0] MemDataReg,
     input wire[31:0] SignExtend1632Out,
-    input wire[31:0] ShiftLeftOut     
-    input wire[4:0] ALUSrcB,
+    input wire[31:0] ShiftLeftOut,     
+    input wire[2:0] ALUSrcB,
 	output reg[31:0] MuxALUSrcBOut
 );
 always @(*) begin
 	case(ALUSrcB)
-		1'b000:
+		3'b000:
 			MuxALUSrcBOut <= RegDataB;
-		1'b001:
+		3'b001:
 			MuxALUSrcBOut <= MemDataReg;
-        	1'b010:
+        3'b010:
 			MuxALUSrcBOut <= SignExtend1632Out;
-		1'b011:
+		3'b011:
 			MuxALUSrcBOut <= 32'd4;
-        	1'b100:
+        3'b100:
 			MuxALUSrcBOut <= ShiftLeftOut;
 	endcase
 end
+endmodule 
