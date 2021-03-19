f9562784154f2ab43f2a73df16c690c8c0dbd204
Matheus Lucena <47263211+matheusepitacio@users.noreply.github.com>
2021-03-18 22:22:45 -0300

    corrigido e testado

--- src/Muxes/MuxMemAdd.v
+++ src/Muxes/MuxMemAdd.v
@@ -1,17 +1,17 @@
 module MuxMemAdd (
     input wire[31:0] PC,
-    input reg[31:0] ExceptionAddress,
-    input reg[31:0] ALUOut,
-    input wire[2:0] MemAdd,
+    input wire[31:0] ExceptionAddress,
+    input wire[31:0] ALUOut,
+    input wire [1:0] MemAdd,
 	output reg[31:0] MuxMemAddOut
 );
 always @(*) begin
 	case(MemAdd)
-		1'b00:
+		2'b00:
 			MuxMemAddOut <= PC;
-		1'b01:
+		2'b01:
 			MuxMemAddOut <= ExceptionAddress;
-		1'b10:
+		2'b10:
 			MuxMemAddOut <= ALUOut;
 	endcase
 end
