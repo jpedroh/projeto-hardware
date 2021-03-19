96785c1918cff4a724a7ae9e21be209b1bf1ce4e
Matheus Lucena <47263211+matheusepitacio@users.noreply.github.com>
2021-03-18 22:24:58 -0300

    corrigido e testado

--- src/Muxes/MuxPCSource.v
+++ src/Muxes/MuxPCSource.v
@@ -2,26 +2,26 @@ module MuxPCSource (
     input wire[31:0] PC,
     input wire[31:0] ALU ,
     input wire[31:0] EPC,
-    input reg[31:0] MemDataReg,
+    input wire[31:0] MemDataReg,
     input wire[31:0] ALUOut,
-    input wire[4:0] PCSource ,
+    input wire[2:0] PCSource ,
 	    output reg[31:0] MuxPCSourceOut
 );
 always @(*) begin
 	case(PCSource)
-		1'b000:
+		3'b000:
 			MuxPCSourceOut <= PC;
-    		1'b001:
+    		3'b001:
 			MuxPCSourceOut <= ALU;
-		1'b010:
+		3'b010:
 			MuxPCSourceOut <= EPC;
-    		1'b011:
+    		3'b011:
 			MuxPCSourceOut <= MemDataReg;
-		1'b100:
+		3'b100:
 			MuxPCSourceOut <= ALUOut;
-        
 	endcase
 end
+endmodule 
 
 
 
