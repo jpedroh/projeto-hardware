6ac49b95d39333bbbe8155dc55e3a724489c3770
Matheus Lucena <47263211+matheusepitacio@users.noreply.github.com>
2021-03-18 22:31:21 -0300

    corrigido e testado

--- src/Muxes/MuxRegDest.v
+++ src/Muxes/MuxRegDest.v
@@ -1,24 +1,25 @@
 module MuxRegDest (
-    input reg[4:0] InstrunctionRegister0,
-    input reg[4:0] InstrunctionRegister1,
-    input reg[4:0] InstrunctionRegister4,
-    input wire[4:0] RegDest ,
-	output reg[31:0] MuxRegDestOut
+    input wire[4:0] InstrunctionRegister0,
+    input wire[4:0] InstrunctionRegister1,
+    input wire[4:0] InstrunctionRegister4,
+    input wire[2:0] RegDest ,
+	output reg[4:0] MuxRegDestOut
 );
 always @(*) begin
 	case(RegDest)
-		1'b000:
+		3'b000:
 			MuxRegDestOut <= InstrunctionRegister0;
-		1'b001:
+		3'b001:
 			MuxRegDestOut <= InstrunctionRegister1;
-    		1'b010:
-			MuxRegDestOut <= 32'd31;
-		1'b011:
-			MuxRegDestOut <= 32'd29;
-    		1'b100:
+    		3'b010:
+			MuxRegDestOut <= 5'd31;
+		3'b011:
+			MuxRegDestOut <= 5'd29;
+    		3'b100:
 			MuxRegDestOut <= InstrunctionRegister4;
 	endcase
 end
+endmodule 
 
 
 
