63d9f6104c9095a74c50d38dc509c087599edf91
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-03-26 15:48:36 -0300

    corrgir excecao no opcode inexistente

--- src/Controle.v
+++ src/Controle.v
@@ -1169,8 +1169,6 @@ module Controle (
             Reg_HI_Write = 1'b0;
             Reg_Lo_Write = 1'b0;
             RegALUOutWrite = 1'b0;
-            ExceptionAddress = 2'b00;
-            RegEPCWrite = 1'b0;
             estado=EXCEPTION_WAIT;
           end
         end
