b76214eab1a577a3be591dcabd500a5cdf97870d
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-03-26 08:28:01 -0300

    remover componentes nao utilizados

--- src/Shifts/ShiftLeft16.v
+++
@@ -1,8 +0,0 @@
-module ShiftLeft16 (
-    input wire[15:0] ShiftLeft16In,
-	output reg[31:0] ShiftLeft16Out
-);
-always @(*) begin
-    ShiftLeft16Out <= ShiftLeft16In << 16;
-end
-endmodule
--- src/Shifts/ShiftLeft2.v
+++
@@ -1,8 +0,0 @@
-module ShiftLeft2 (
-    input wire[31:0] ShiftLeft2In,
-	output reg[31:0] ShiftLeft2Out
-);
-always @(*) begin
-    ShiftLeft2Out <= ShiftLeft2In << 2;
-end
-endmodule
